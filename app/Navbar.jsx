8914688a91178606dc2837ba648146f2067ddc65
Your Name <lebomariri505@gmail.com>
2024-01-13 15:30:47 +0200

    dropdown menu avatar

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { FaWineGlassAlt } from "react-icons/fa";
 import { usePathname } from 'next/navigation';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react';
 
 import {
   Box,
@@ -14,51 +15,41 @@ import {
   Text,
 } from '@radix-ui/themes';
 
-const Navbar = () => {
+const NavBar = () => {
+  const pathname = usePathname();
+  const { data: session } = useSession();
+
+  const links = [
+    {
+      id: 1,
+      href: '/',
+      label: 'Dashboard',
+    },
+    {
+      id: 2,
+      href: '/wines',
+      label: 'Wines',
+    },
+  ];
+
   return (
     <nav className="py-3 border-b px-5 mb-5">
     <Container>
       <Flex justify="between">
-        <Flex gap="3" align="center">
+      <Flex gap="3" align="center">
           <Link href="/">
-            <FaWineGlassAlt className="text-2xl" />
-          </Link>
-          <NavLinks />
-        </Flex>
-        {/* <AuthStatus /> */}
-      </Flex>
-    </Container>
-  </nav>
-  )
-}
-
-export default Navbar
-
-const NavLinks = () => {
-    const pathname = usePathname();
-  
-    const links = [
-      {
-        id: 1,
-        href: '/',
-        label: 'Dashboard',
-      },
-      {
-        id: 2,
-        href: '/wines',
-        label: 'Wines',
-      },
-    ];
-  
-    return (
+        <FaWineGlassAlt className="text-2xl" />
+      </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
           <li key={link.id}>
             <Link
               href={link.href}
+              passHref
               className={classnames({
-                'nav-link': true,
-                '!text-zinc-950': link.href === pathname,
+                'text-zinc-950': link.href === pathname,
+                'text-zinc-600': link.href !== pathname,
+                'hover:text-zinc-800 transition-colors': true,
               })}
             >
               {link.label}
@@ -66,5 +57,35 @@ const NavLinks = () => {
           </li>
         ))}
       </ul>
-    );
-  };
+      </Flex>
+      <Box>
+      {session && (
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Avatar
+                  src={session.user?.image}
+                  fallback="?"
+                  size="3"
+                  radius="full"
+                  className="cursor-pointer"
+                ></Avatar>
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Label>
+                  <Text size="2">{session.user?.email}</Text>
+                </DropdownMenu.Label>
+                <DropdownMenu.Item>
+                  <Link href="/api/auth/signout">Log out</Link>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          )}
+          {!session && <Link href="/api/auth/signin">Login</Link>}
+       </Box>
+      </Flex>
+      </Container>
+      </nav>
+  );
+};
+
+export default NavBar;
